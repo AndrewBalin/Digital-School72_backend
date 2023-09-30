e842ac6c0c0bad430505811f31f169fe3c970eee
MartellOnell <mt102005@mail.ru>
2023-09-30 17:21:36 +0500

    rework user manager (api/models.py)
    add role field and comment method create_superuser (we wont use django base admin)

--- api/models.py
+++ api/models.py
@@ -8,43 +8,61 @@ from django.contrib.auth.models import (
 import jwt
 from jwt import *
 
+ROLES = {
+    '0': 'unconfirmed',
+    '1': 'student',
+    '2': 'teacher',
+    '3': 'school admin',
+    '-1': 'platform admin'
+}
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, email, password=None):
+    def create_user(self, username, email, password, surname, patronymic, role_code):
         if username is None:
-            raise TypeError('Users must have name')
-
+            raise TypeError('User must have username')
+        
         if email is None:
-            raise TypeError('Users must have email address.')
+            raise TypeError('User must have email')
+        
+        if password is None:
+            raise TypeError('User must have password')
+        
+        if patronymic is None:
+            raise TypeError('User must have patronymic')
+        
+        if surname is None:
+            raise TypeError('User must have surname')
+        
+        role_code = int(role_code)
+        if not(-1 <= role_code <= 3) or role_code is None:
+            raise TypeError('Unknow role code')
+        role = ROLES[str(role_code)]
 
-        user = self.model(username=username, email=self.normalize_email(str(email)))
+
+        user = self.model(username=username, 
+                          email=self.normalize_email(str(email)), 
+                          surname=surname, 
+                          patronymic=patronymic,
+                          role=role)
         user.set_password(str(password))
         user.save()
 
         return user
 
-    def create_superuser(self, username, email, password):
+    # def create_superuser(self, username, email, password):
 
-        if password is None:
-            raise TypeError('Superuser must have password')
+    #     if password is None:
+    #         raise TypeError('Superuser must have password')
 
-        user = self.create_user(username, email, password)
-        user.is_superuser = True
-        user.is_staff = True
-        user.save()
+    #     user = self.create_user(username, email, password)
+    #     user.is_superuser = True
+    #     user.is_staff = True
+    #     user.save()
 
-        return user
+    #     return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    PERMISSIONS_CHOICES = [
-        (0, 'unconfirmed'),
-        (1, 'student'),
-        (2, 'teacher'),
-        (3, 'school admin'),
-        (-1, 'platform admin'),
-    ]
-
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -55,6 +73,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
     patronymic = models.CharField(max_length=255, null=True)
+    role = models.CharField(max_length=14)
 
     classes = models.ManyToManyField("SchoolClass")
     schools = models.ManyToManyField("School")
