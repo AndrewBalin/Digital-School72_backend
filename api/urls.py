b8bbcdd6268d030d660110331b60c4db61aba8e9
MartellOnell <mt102005@mail.ru>
2023-10-04 10:18:48 +0500

    add urls for new views functions

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,8 @@ from api import views
 urlpatterns = [
     path('schoolclass/<int:id>', views.get_class_data),
     path('login', views.login),
-    path('get_csrf_token', views.get_csrf_token_view)
+    path('get_csrf_token', views.get_csrf_token_view),
+    path('get_user_name/<int:id>', views.get_user_name),
+    path('get_user_email/<int:id>', views.get_user_email),
+    path('get_user/<int:id>', views.get_user_profile)
 ]
