05253c6ddc74cecfddeb3906c6dfe4542ab13056
MartellOnell <mt102005@mail.ru>
2023-10-08 21:56:54 +0500

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('schoolclass/<int:id>', views.get_class_data),
+    path('get_schoolclass/<int:id>', views.get_class_data),
     path('login', views.login),
     path('get_csrf_token', views.get_csrf_token_view),
     path('get_user_name/<int:id>', views.get_user_name),
